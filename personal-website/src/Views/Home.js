867c01a74928801964c57af38d964bd48397a872
Lamberto <lambertoragnolini9@gmail.com>
2024-10-28 10:49:40 +0100

    initial commit with the first content of the page displayed

---
+++ personal-website/src/Views/Home.js
@@ -0,0 +1,15 @@
+
+export default function Home() {
+        return (
+            
+                <div className="w-full flex flex-row p-2">
+                    <div className="w-1/4 rounded-[50px]">
+                        <img src="/images/NORMAL_FORMAL.jpg" alt="foto" className="w-full object-cover rounded-[50px]"></img>
+                    </div>
+                    <div className="w-3/4 justify-center m-auto p-2">
+                        <p className="italic font-bold text-black text-center"> Io sono lamberto uno studente che ha appenainiziatonquesto sito e ha deciso che adesso non cia sara piun scritto nuente di sensato questo e solo un modo oer vedere quanto ruescoe a scriver bene senza guardare la tastiersa e sembra che sdtia anadando bene anche se ogni tanto sbaglo non me </p>
+                    </div>
+                </div>
+            
+        );
+}
